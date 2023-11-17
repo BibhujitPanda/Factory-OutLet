a668dd9d2bcaefce655b23f809f913a8fe783cbc
BibhujitPanda <129971304+BibhujitPanda@users.noreply.github.com>
2023-11-17 05:44:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+// Load Details
+
+function toggleSelection(element) {
+    // Toggle the 'selected' class on the clicked <li>
+    element.classList.toggle('selected');
+}
+
+function showEnterPo() {
+    // Add logic to display content related to "Enter Your Po"
+    console.log("Showing content for Enter Your Po");
+}
+
+function showBookingPlaced() {
+    // Add logic to display content related to "Booking Placed"
+    console.log("Showing content for Booking Placed");
+}
+
+function showFulfilledByPremiours() {
+    // Add logic to display content related to "Fulfilled by Premiours"
+    console.log("Showing content for Fulfilled by Premiours");
+}
